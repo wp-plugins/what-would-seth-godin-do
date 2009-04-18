a4a8c401dac94484a1ee8d2bee42ef7cbfa81974
richardkmiller <richardkmiller@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-04-18 21:27:46 +0000

    Committing WWSGD version 1.3, for history's sake.
    git-svn-id: https://plugins.svn.wordpress.org/what-would-seth-godin-do/tags/1.3@111606 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- what_would_seth_godin_do.php
+++ what_would_seth_godin_do.php
@@ -4,17 +4,17 @@
 Plugin Name: What Would Seth Godin Do
 Plugin URI: http://www.richardkmiller.com/blog/wordpress-plugin-what-would-seth-godin-do/
 Description: Displays a custom welcome message to new visitors
-Version: 1.0
+Version: 1.3
 Author: Richard K Miller
 Author URI: http://www.richardkmiller.com/blog/
 
-Copyright (c) 2006 Richard K Miller
+Copyright (c) 2006-2007 Richard K Miller
 Released under the GNU General Public License (GPL)
 http://www.gnu.org/licenses/gpl.txt
 */
 
 $defaultdata = array(
-	'new_visitor_message' => "<p style='border:thin dotted black; padding:3mm;'>If you're new here, you may want to subscribe to my <a href='".get_option('siteurl')."/feed/'>RSS feed</a>.  Thanks for visiting!</p>",
+	'new_visitor_message' => "<p style='border:thin dotted black; padding:3mm;'>If you're new here, you may want to subscribe to my <a href='".get_option('home')."/feed/'>RSS feed</a>.  Thanks for visiting!</p>",
 	'message_location' => 'before_post',
 	'repetition' => '3'
 	);
@@ -61,7 +61,7 @@ function wwsgd_options_subpanel()
 		<p><input type="radio" name="message_location" value="before_post" <?php if ($wwsgd_settings['message_location'] == 'before_post') echo 'checked="checked"'; ?> /> Before Post</p>
 		<p><input type="radio" name="message_location" value="after_post" <?php if ($wwsgd_settings['message_location'] == 'after_post') echo 'checked="checked"'; ?> /> After Post</p>
 		<h3>Repetition</h3>
-		<p>Show the message the first <input type="text" name="repetition" value="<?php echo $wwsgd_settings['repetition']; ?>" size="3" /> times the user visits.</p>
+		<p>Show the message the first <input type="text" name="repetition" value="<?php echo $wwsgd_settings['repetition']; ?>" size="3" /> times the user visits your blog. (Enter "0" to always display the message.)</p>
 		<p><input type="submit" name="submit" value="Save Settings" /></p>
 		</form>
 		</div>
@@ -82,17 +82,18 @@ function wwsgd_set_cookie()
 		{
 			$wwsgd_visits = 1;
 		}
-		$url = parse_url(get_option('siteurl'));
+		$url = parse_url(get_option('home'));
 		setcookie('wwsgd_visits', $wwsgd_visits, time()+60*60*24*365, $url['path'] . '/');
 	}
 }
 
 function wwsgd_message_filter($content = '')
 {
-	global $wwsgd_visits, $wwsgd_settings;
+	global $wwsgd_visits, $wwsgd_settings, $wwsgd_messagedisplayed;
 	
-	if ($wwsgd_visits <= $wwsgd_settings['repetition'] && !is_feed())
+	if (($wwsgd_visits <= $wwsgd_settings['repetition'] || 0 == $wwsgd_settings['repetition']) && !is_feed() && !$wwsgd_messagedisplayed)
 	{
+		$wwsgd_messagedisplayed = true;
 		if ($wwsgd_settings['message_location'] == 'before_post')
 		{
 			return
