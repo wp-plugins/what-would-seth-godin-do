af80bf15f4ad43baab92337845f3302544a2083b
richardkmiller <richardkmiller@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-10 01:15:21 +0000

    WWSGD 2.0.2. Works with non-standard installations. Tested with WordPress 3.2
    git-svn-id: https://plugins.svn.wordpress.org/what-would-seth-godin-do/tags/2.0.2@407666 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- what_would_seth_godin_do.php
+++ what_would_seth_godin_do.php
@@ -4,7 +4,7 @@
 Plugin Name: What Would Seth Godin Do
 Plugin URI: http://richardkmiller.com/wordpress-plugin-what-would-seth-godin-do
 Description: Displays a custom welcome message to new visitors and another to return visitors.
-Version: 1.7.3
+Version: 2.0.2
 Author: Richard K Miller
 Author URI: http://richardkmiller.com/
 
@@ -16,7 +16,7 @@ http://www.gnu.org/licenses/gpl.txt
 $wwsgd_settings = wwsgd_initialize_and_get_settings();
 
 add_action('admin_menu', 'wwsgd_options_page');
-add_action('wp_head', 'wwsgd_js');
+add_action('wp_footer', 'wwsgd_js');
 add_filter('the_content', 'wwsgd_filter_content');
 add_filter('get_the_excerpt', 'wwsgd_filter_excerpt', 1);
 wp_enqueue_script('jquery');
@@ -200,17 +200,19 @@ function wwsgd_the_message() {
 function wwsgd_js() {
     global $wwsgd_settings;
 ?>
-<script type="text/javascript" src="<?php echo bloginfo("url"); ?>/wp-content/plugins/what-would-seth-godin-do/jquery.cookie.js"></script>
+<script type="text/javascript" src="<?php echo WP_PLUGIN_URL; ?>/what-would-seth-godin-do/jquery.cookie.js"></script>
 <script type="text/javascript">
     jQuery(document).ready(function() {
+        var count;
         if ( !jQuery.cookie('wwsgd_visits') ) {
-            jQuery.cookie('wwsgd_visits', 1);
+            count = 1;
         }
         else {
-            jQuery.cookie('wwsgd_visits', parseInt(jQuery.cookie('wwsgd_visits'), 10) + 1);
+            count = parseInt(jQuery.cookie('wwsgd_visits'), 10) + 1;
         }
+        jQuery.cookie('wwsgd_visits', count, { expires: 365 });
 
-        if ( parseInt(jQuery.cookie('wwsgd_visits'), 10) <= <?php echo $wwsgd_settings['repetition'] ?> ) {
+        if ( count <= <?php echo $wwsgd_settings['repetition'] ?> ) {
             jQuery(".wwsgd").show();
         }
     });
